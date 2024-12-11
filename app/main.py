3b15708386fdcb6c347ec92c42aca245f1082a41
Anastasiia <skubakovaa@gmail.com>
2024-12-11 21:57:07 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,66 @@
-class Validator:
-    pass
+from abc import abstractmethod, ABC
 
 
-class Number:
-    pass
+class Validator(ABC):
+    def __set_name__(self, owner: type, name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance: object, owner: type) -> None:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance: object, value: object) -> None:
+        self.validate(value)
+        setattr(instance, self.protected_name, value)
+
+    @abstractmethod
+    def validate(self, value: object) -> None:
+        pass
 
 
-class OneOf:
-    pass
+class Number(Validator):
+    def __init__(self, min_value: int, max_value: int) -> None:
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value: object) -> None:
+        if not isinstance(value, int):
+            raise TypeError("Quantity should be integer.")
+        if value not in range(self.min_value, self.max_value + 1):
+            raise ValueError(f"Quantity should not be less than "
+                             f"{self.min_value} and greater than "
+                             f"{self.max_value}.")
+
+
+class OneOf(Validator):
+    def __init__(self) -> None:
+        self.options = "ketchup", "mayo", "burger"
+
+    def validate(self, value: object) -> None:
+        if value not in self.options:
+            raise ValueError(f"Expected {value} "
+                             f"to be one of {self.options}.")
 
 
 class BurgerRecipe:
-    pass
+    buns = Number(2, 3)
+    cheese = Number(0, 2)
+    tomatoes = Number(0, 3)
+    cutlets = Number(1, 3)
+    eggs = Number(0, 2)
+    sauce = OneOf()
+
+    def __init__(
+            self,
+            buns: int,
+            cheese: int,
+            tomatoes: int,
+            cutlets: int,
+            eggs: int,
+            sauce: str
+    ) -> None:
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
